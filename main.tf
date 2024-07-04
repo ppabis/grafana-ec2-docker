72592ecfccde49d87b4ff418585d3915529cbf18
ppabis <piotr@pabis.eu>
2024-07-04 13:54:03 +0200

    Added http, null and random providers

--- main.tf
+++ main.tf
@@ -8,5 +8,20 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
   }
 }
