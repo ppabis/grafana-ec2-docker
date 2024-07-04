d18e9cc6b5b1b0ccc871380938f9d10de2e7ed77
ppabis <piotr@pabis.eu>
2024-07-04 14:24:49 +0200

    More IAM names changes

--- iam.tf
+++ iam.tf
@@ -4,10 +4,10 @@
 
 resource "aws_iam_instance_profile" "grafana_ec2_profile" {
   name = "grafana_ec2-${random_string.stack_id.result}"
-  role = aws_iam_role.grafana_ec2_profile.name
+  role = aws_iam_role.grafana_ec2_role.name
 }
 
-resource "aws_iam_role" "grafana_ec2_profile" {
+resource "aws_iam_role" "grafana_ec2_role" {
   name = "grafana_ec2-${random_string.stack_id.result}-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -22,11 +22,11 @@ resource "aws_iam_role" "grafana_ec2_profile" {
 }
 
 resource "aws_iam_role_policy_attachment" "ssm" {
-  role       = aws_iam_role.grafana_ec2_profile.name
+  role       = aws_iam_role.grafana_ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role_policy_attachment" "timestream" {
-  role       = aws_iam_role.grafana_ec2_profile.name
+  role       = aws_iam_role.grafana_ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonTimestreamReadOnlyAccess"
 }
