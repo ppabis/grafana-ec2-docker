2ebd51c427e5be1ba9e394cdffb3e79d4214a889
ppabis <piotr@pabis.eu>
2024-07-04 17:27:47 +0200

    The CloudFormation outputs are not needed

--- data.tf
+++ data.tf
@@ -1,11 +1,3 @@
-data "aws_cloudformation_export" "habitica_stats_database" {
-  name = "HabiticaStatsDatabaseName"
-}
-
-data "aws_cloudformation_export" "habitica_stats_table" {
-  name = "HabiticaStatsTableName"
-}
-
 data "aws_ssm_parameter" "al2023" {
   name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-arm64"
 }
